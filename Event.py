74bfa400117815905df57bb46377925b2704f826
alekssokolov164 <alekssokolov164@gmail.com>
2024-01-16 11:11:47 +0500

    Calendar, Event, User

--- Event.py
+++ Event.py
@@ -20,21 +20,28 @@ import datetime
 class Event:
     '''Создаем событие поэлементно: название, описание,  дата начала, дата завершения, периодичность,
      список участников'''
-
+    _author: str = ""
     _name = ""
     _description = ""
     _ets = datetime.datetime
     _eta = datetime.datetime
-    _frequency = dict()
+    _period = dict()
     _users = list()
 
-    def __init__(self):
-        self._ets = datetime.datetime
-        self._eta = datetime.datetime
-        self._name = ""
-        self._description = ""
-        self._users = list()
-        self._frequency = dict()
+    def __init__(self, author, name, description, ets, eta, users, period):
+        self._author = author
+        self._ets = ets
+        self._eta = eta
+        self._name = name
+        self._description = description
+        self._users = users
+        self._period = period
+
+    def edit_author(self, author):
+        self._author = author
+
+    def get_author(self):
+        return self._author
 
     def edit_name(self, new_name):
         self._name = new_name
@@ -42,22 +49,34 @@ class Event:
     def edit_description(self, new_description):
         self._description = new_description
 
+    def get_description(self):
+        return self._description
+
     def edit_ets(self, ets):
         self._ets = ets
+
+    def get_ets(self):
+        return self._ets
 
     def edit_eta(self, eta):
         self._eta = eta
 
-    def edit_period(self, a, b):
-        self._frequency = {a: b}
-
-    def add_users(self, user):
-        if user is not self._users:
-            self._users.append(user)
+    def get_eta(self):
+        return self._eta
 
     def get_timings(self):
         return self._ets, self._eta
 
+    def edit_period(self, a, b):
+        self._period = {a: b}
+
+    def get_period(self):
+        return self._period
+
+    def add_users(self, users):
+        for user in users:
+            if user is not self._users:
+                self._users.append(user)
 
     def get_users(self):
         return self._users
@@ -69,12 +88,9 @@ class Event:
     def del_user(self, user):
         return self._users.remove(user)
 
-
-
-
-
     def __str__(self):
         return (f"Событие {self._name},\n {self._description},\n дата и время начала: {self._ets},\n "
                 f"дата и время завершения: {self._eta}\n")
+
     def __repr__(self):
         return f"{self._name}"
