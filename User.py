d9ee388850dfa612a833fa0e2b6d62b6236cc8fe
alekssokolov164 <alekssokolov164@gmail.com>
2024-01-16 11:42:36 +0500

    2

--- User.py
+++ User.py
@@ -4,6 +4,7 @@
 """
 import Calendar
 
+
 class User:
     _login = ""
     _pasword = ""
@@ -11,24 +12,27 @@ class User:
     _plan = Calendar.Calendar()
     _message = ""
 
-    def __init__(self, login, pasword):
+    def __init__(self, login, pasword, plan):
         self._id = "@" + str(self.__class__.__id_counter__)
         self.__class__.__id_counter__ += 1
         self._login = login
         self._pasword = pasword
-        self._plan = Calendar.Calendar()
+        self._plan = plan
 
-    def get_2(self):
-        return self._login, self._pasword
+    def get_login(self):
+        return self._login
 
     def get_pasword(self):
         return self._pasword
 
+    def get_id(self):
+        return self._id
+
     def __str__(self):
-        return f" {self._id}\n"
+        return f" Пользователь {self._id}\n"
 
     def __repr__(self):
-        return f"Пользователь {self._id}]"
+        return f"{self._id}]"
 
     def __hash__(self):
         return int(self._id[1:])
