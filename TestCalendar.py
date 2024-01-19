f1b94ad2d0c886de78d1efdf6d692fc16a23dc67
alekssokolov164 <alekssokolov164@gmail.com>
2024-01-19 10:40:04 +0500

    All \ month

--- TestCalendar.py
+++ TestCalendar.py
@@ -13,18 +13,19 @@ c.append_event(ev1)
 print(c)
 x2 = datetime.datetime(2024, 1, 13, 23, 59, 59)
 y2 = datetime.datetime(2024, 1, 14, 00, 00, 1)
-c.add_event("@2", "Старый Новый год", "Ёллллка", x2, y2, [1, 1, 1], 4)
+c.add_event("@2", "Старый Новый год", "Ёллллка", x2, y2, [1, 1, 1], 2)
 print(c)
-x4 = datetime.datetime(2024, 2, 17, 00, 00, 00)
-y4 = datetime.datetime(2024, 2, 18, 00, 00, 1)
-c.add_event("@3", "ДР", "Нет Ёллллка", x2, y2, [1, 1, 1], 3)
+x4 = datetime.datetime(2024, 1, 1, 00, 00, 10)
+y4 = datetime.datetime(2024, 1, 1, 22, 59, 59)
+c.add_event("@3", "ДР", "Нет Ёллллка", x4, y4, [1, 1, 1], 1)
 print(c)
 
-x3 = datetime.datetime(2023, 12, 20, 23, 59, 59)
-y3 = datetime.datetime(2025, 3, 14, 00, 00, 1)
-print(c.search_events(x3, y3))
+x3 = datetime.datetime(2024, 1, 1, 00, 00, 10)
+y3 = datetime.datetime(2024, 1, 7, 23, 00, 10)
+c.search_events(x3, y3)
 for i in c.search_events(x3, y3):
     print(i)
+
 
 # class TestCalendar(unittest.TestCase):
 #
