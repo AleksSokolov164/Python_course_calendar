0450a6ac3b1bab3468a3778fa53ba23a924b4280
alekssokolov164 <alekssokolov164@gmail.com>
2024-03-04 21:18:25 +0500

    flet - авторизация работает

--- flet_window.py
+++ flet_window.py
@@ -2,6 +2,7 @@ import flet as ft
 import User
 import csv
 
+
 def main(page):
     users = dict()
 
@@ -16,7 +17,6 @@ def main(page):
             user = User.User(login=login, password=password)
             users[login] = user
 
-
     def password_click(e):
         if txt_password.value == users[txt_login.value].get_password():
             row2.visible = not row2.visible
@@ -24,3 +24,26 @@ def main(page):
         else:
             txt_password.error_text = "Пароль не подходит"
             page.update()
+
+    def login_click(e):
+        if txt_login.value in users.keys():
+            row1.visible = not row1.visible
+            page.add(row2)
+        else:
+            txt_login.error_text = "Такой логин отсутствует"
+            page.update()
+
+    txt_login = ft.TextField(label="Введите ваш логин")
+    click_1 = ft.ElevatedButton("Ввод", on_click=login_click)
+    txt_password = ft.TextField(label="Введите ваш пароль")
+    click_2 = ft.ElevatedButton("Ввод", on_click=password_click)
+
+    txt_end = ft.Text("Вы прошли авторизацию")
+
+    row1 = ft.Row([txt_login, click_1])
+    row2 = ft.Row([txt_password, click_2])
+
+    page.add(row1)
+
+
+ft.app(target=main)
